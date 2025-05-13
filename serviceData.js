fd971d29f0fde7665f9da356167c11ba986f52ab
sid5862 <81680001+sid5862@users.noreply.github.com>
2025-05-13 11:13:13 +0530

    Add files via upload

---
+++ serviceData.js
@@ -0,0 +1,226 @@
+export const servicesdata = [
+  {
+    id: 1,
+    name: "Digital X ray",
+    short_desc:
+      "An X-ray is a type of medical imaging that uses radiation to take pictures of the inside of your body.",
+    image: "/services/digital-xray.jpeg",
+    url: "digital-xray",
+    imageurl: "digitalXRay",
+    content: [
+      {
+        id: 11,
+        ques: "Overview",
+        ans: "",
+      },
+      {
+        id: 12,
+        ques: "What is an X-ray?",
+        ans: `An X-ray is a type of medical imaging that uses radiation to take pictures of the inside of your body. 
+Most people use the term “X-ray” to mean a plain radiograph (plain film), or just one image. But X-rays are also the type of radiation that imaging machines use to create pictures. X-ray radiation is also used in other types of medical imaging, like CT scans, to get many images that are interpreted by a computer to form 3D pictures. This piece will focus on plain X-rays.
+`,
+      },
+      {
+        id: 13,
+        ques: "How do I prepare for an X-ray?",
+        ans: `Preparation for an X-ray depends on the type of X-ray you’re getting. Your provider may ask you to:
+Avoid using lotions, creams or perfume. These can cause shadows on your X-rays and make the results inaccurate.
+Remove metal objects like jewelry, hairpins or hearing aids. Metal can interfere with X-rays and make the results inaccurate.
+Stop eating or drinking several hours beforehand.
+Wear comfortable clothing or change into a gown before the X-ray.
+Tell your healthcare provider about your health history, allergies and any medications you’re taking. If you’re pregnant, think you might be pregnant or are breastfeeding (chestfeeding), tell your provider before getting an X-ray. If you’re getting an X-ray with contrast, let your provider know if you have a kidney condition.
+`,
+      },
+    ],
+  },
+  {
+    id: 2,
+    name: "Ultrasound",
+    short_desc:
+      "Non-invasive scan which uses high frequency sound waves of the inside of your body.",
+    image: "/services/ultrasound.png",
+    url: "ultrasound",
+    imageurl: "ultrasound.jpg",
+    content: [
+      {
+        id: 21,
+        ques: "What is an Ultrasound scan?",
+        ans: `Ultrasound is a non-invasive scan which uses high frequency sound waves of the inside of your body. During your private Ultrasound scan, a probe is passed over the skin and this emits sound waves. These sound waves bounce off your organs and send a signal via the microphone to the computer which then displays the sound as an image.`,
+      },
+      {
+        id: 22,
+        ques: "How do I prepare for an ultrasound?",
+        ans: `<p style='margin:0cm;font-size:13px;font-family:"Calibri",sans-serif;'><span style="font-size: 17px;">The steps you will take to prepare for an ultrasound will depend on the area or organ that is being examined.</span></p>
+<p style='margin:0cm;font-size:13px;font-family:"Calibri",sans-serif;'><span style="font-size: 17px;">Your doctor may tell you to fast for 4 to 6 hours before your ultrasound, especially if your abdomen is being examined. Undigested food can block the sound waves, making it difficult for the technician to get a clear picture.</span></p>
+<p style='margin:0cm;font-size:13px;font-family:"Calibri",sans-serif;'><span style="font-size: 17px;">For an examination of the gallbladder, liver, pancreas, or spleen, you may be told to eat a fat-free meal the evening before your test and then to fast until the procedure. However, you can continue to drink water and take any medications as instructed. For other examinations, you may be asked to drink a lot of water and to hold your urine so that your bladder is full and better visualized.</span></p>
+<p style='margin:0cm;font-size:13px;font-family:"Calibri",sans-serif;'><span style="font-size: 17px;">&nbsp;</span></p>
+<p style='margin:0cm;font-size:13px;font-family:"Calibri",sans-serif;'><strong><span style="font-size: 17px;">As Ultrasound scans use high frequency sound waves, they are an extremely safe and convenient procedure. The patient is fully awake during the procedure and no discomfort is experienced.</span></strong></p>`,
+      },
+    ],
+  },
+  {
+    id: 3,
+    name: "Color Doppler",
+    short_desc:
+      "Color Doppler is an advanced ultrasound imaging technique used to visualize blood flow in the body. It combines traditional ultrasound imaging with Doppler ultrasound, using color coding to display the direction and speed of blood flow.",
+    image: "/services/colordoppler/colordoppler.jpg",
+    url: "colordoppler",
+  },
+  {
+    id: 4,
+    name: "Liver Fibroscan",
+    url: "liver-fibroscan",
+    short_desc:
+      "The FibroScan is a noninvasive way to gauge the health of your liver and determine what stage of liver disease you and your care team might be up against.",
+    image: "/services/liverfibroscan.png",
+    content: [
+      {
+        id: 41,
+        ques: "What is Fibroscan?",
+        ans: `<p style='margin-top:0in;margin-right:0in;margin-bottom:10.0pt;margin-left:0in;line-height:115%;font-size:15px;font-family:"Calibri",sans-serif;'><span style="font-family: Calibri, sans-serif; font-size: 20px; color: rgb(0, 0, 0);">The FibroScan is a noninvasive way to gauge the health of your liver and determine what stage of liver disease you and your care team might be up against. Earlier, there was only invasive biopsy to measure the liver stiffness. With the FibroScan, liver stiffness is measured, which indicates whether or not scarring is present, and which can also indicate the extent of the scarring.&nbsp;</span></p> <p style='margin-top:0in;margin-right:0in;margin-bottom:10.0pt;margin-left:0in;line-height:115%;font-size:15px;font-family:"Calibri",sans-serif;'><span style="font-family: Calibri, sans-serif; font-size: 20px; color: rgb(0, 0, 0);">Scarring of the liver in the initial stages is called fibrosis, and more severe scarring is cirrhosis. The FibroScan is like a fibrosis scan that can use scarring measurements to provide a diagnosis or to track the progression of liver disease.</span></p>`,
+      },
+
+      {
+        id: 42,
+        ques: "How do I prepare for my FibroScan?",
+        ans: `<p style='margin-top:0in;margin-right:0in;margin-bottom:10.0pt;margin-left:0in;line-height:115%;font-size:15px;font-family:"Calibri",sans-serif;'><span style="font-family: Calibri, sans-serif; font-size: 20px; color: rgb(0, 0, 0);">FibroScan preparation is simple and straightforward. On the day of your procedure, do not eat or drink anything <strong><em>3 hours before your test</em></strong>. You should wear comfortable clothes (e.g. shirt, loose dress) that can be moved/repositioned to allow the nurse to examine your right upper abdomen. Please do not wear any tight clothing to your appointment.</span></p> <p><strong><span style="font-size: 20px; line-height: 115%; font-family: Calibri, sans-serif; color: rgb(0, 0, 0);">GOOD NEWS: FIBROSCAN IS PAINLESS TEST.</span></strong></p>`,
+      },
+      {
+        id: 43,
+        ques: "What should I expect during the FibroScan? ",
+        ans: `<p style='margin-top:0in;margin-right:0in;margin-bottom:10.0pt;margin-left:0in;line-height:115%;font-size:15px;font-family:"Calibri",sans-serif;'><span style="font-size: 20px; line-height: 115%; font-family: Calibri, sans-serif; color: rgb(0, 0, 0);">During the exam, you will be lying down on your back with the right arm raised and tucked behind your head. Preparation includes no food or drink three hours before the exam and you may wish to wear a looser fitting shirt as the skin covering your right rib cage area will need to be exposed in order to complete the exam. A doctor will place a probe, on your side near where your liver is. That probe sends painless sound vibration through your body and into your liver. The probe measures how fast it takes the vibration to travel through the liver. The faster it takes the vibration to travel through the liver, the stiffer the liver is. The examination itself takes about 10-15 minutes. You should plan to spend about 45 minutes at the clinic total. You may drive yourself to and from the test.</span></p>`,
+      },
+      {
+        id: 44,
+        // ques: "What is Fibroscan?",
+        ans: `<p style='margin-top:0in;margin-right:0in;margin-bottom:10.0pt;margin-left:0in;line-height:115%;font-size:15px;font-family:"Calibri",sans-serif;'><strong><span style="font-family: Calibri, sans-serif; font-size: 20px; color: rgb(0, 0, 0);">Your physician may recommend a FibroScan test if you have one of the following chronic liver conditions:</span></strong></p>
+<ul style="list-style-type: disc;">
+    <li style="font-size: 20px;">Autoimmune Hepatitis</li>
+    <li style="font-size: 20px;">Cirrhosis</li>
+    <li style="font-size: 20px;">Genetic Diseases (such as Hemochromatosis and Wilson&rsquo;s Disease)</li>
+    <li style="font-size: 20px;">Hepatitis B</li>
+    <li style="font-size: 20px;">Hepatitis C</li>
+    <li style="font-size: 20px;">Alcoholic Liver Disease</li>
+    <li style="font-size: 20px;">Non-Alcoholic Steatohepatitis (NASH)</li>
+</ul>`,
+      },
+      {
+        id: 45,
+        ques: "What should I expect after the FibroScan?",
+        ans: `<p style='margin-top:0in;margin-right:0in;margin-bottom:10.0pt;margin-left:0in;line-height:115%;font-size:15px;font-family:"Calibri",sans-serif;'><span style="font-family: Calibri, sans-serif; color: rgb(0, 0, 0); font-size: 20px;">The FibroScan computer will analyze the information from your test. </span></p>`,
+      },
+    ],
+  },
+  {
+    id: 5,
+    name: "Breast elastography",
+    short_desc:
+      "Elastography is a specialized ultrasound technique used to measure the stiffness or elasticity of tissue, often used to detect abnormalities in organs like the liver, breast, and thyroid.",
+    image: "/services/breastelastography.png",
+    url: "breast-elastography",
+  },
+  {
+    id: 6,
+    name: "ECG",
+    image: "/services/ecg.png",
+    short_desc:
+      "An Electrocardiogram (ECG or EKG) is a medical test that records the electrical activity of the heart.",
+    url: "ecg/ecg-testing",
+  },
+  {
+    id: 7,
+    name: "24 Hours Holter Monitoring",
+    short_desc:
+      "24-hour Holter monitoring is a type of continuous ECG (electrocardiogram) that records the electrical activity of your heart over a full 24-hour period.",
+    image: "/services/holter.png",
+    url: "holter-monitoring",
+  },
+  {
+    id: 8,
+    name: "ECHO",
+    short_desc:
+      "2D ECHO, also known as 2D Echocardiography, is a non-invasive ultrasound test that uses sound waves to create real-time images of the heart.",
+    image: "/services/echo.jpeg",
+    url: "echo/2d_echo",
+  },
+  {
+    id: 9,
+    name: "TMT",
+    short_desc:
+      "TMT (Treadmill Test), also known as a stress test, is a diagnostic procedure used to assess how well the heart functions during physical exertion.",
+    image: "/services/tmt.png",
+    url: "tmt",
+  },
+  {
+    id: 10,
+    name: "Ambulatory BP monitoring (ABPM)",
+    short_desc:
+      "Ambulatory blood pressure monitoring (ABPM) is a method used to measure blood pressure over a 24-hour period while a patient goes about their normal daily activities.",
+    image: "/services/abpm.jpeg",
+    url: "ambulatory",
+  },
+  {
+    id: 11,
+    name: "Ultrasound Guided Interventions",
+    short_desc:
+      "Fine Needle Aspiration Cytology (FNAC) is a diagnostic procedure used to investigate lumps or abnormalities in the body by collecting a small sample of tissue or fluid using a thin, hollow needle.",
+    image: "/services/ultrasoundguidedintervention.jpeg",
+    url: "/ultrasoundguided/fnac",
+  },
+  {
+    id: 12,
+    name: "Pulmonary function test (PFT)",
+    short_desc:
+      "A Pulmonary Function Test (PFT) is a set of non-invasive tests used to measure how well your lungs are functioning.",
+    image: "/services/pulmunory.png",
+    url: "pulmonary-function-test",
+  },
+  {
+    id: 13,
+    name: "Uroflow Metry",
+    short_desc:
+      "Uroflowmetry is a diagnostic test that measures the flow rate and pattern of urine as it is passed from the bladder through the urethra.",
+    image: "/services/uroflowmetry.png",
+    url: "uroflowmetry",
+  },
+  {
+    id: 14,
+    name: "Electroencephalogram (EEG)",
+    short_desc:
+      "An Electroencephalogram (EEG) is a diagnostic test that measures the electrical activity of the brain. It records brain wave patterns through small electrodes placed on the scalp.",
+    image: "/services/eeg.png",
+    url: "eeg",
+  },
+  {
+    id: 15,
+    name: "NCV",
+    short_desc:
+      "Nerve Conduction Velocity (NCV) is a diagnostic test that measures the speed at which electrical signals travel along a nerve.",
+    image: "/services/ncv.png",
+    url: "ncv",
+  },
+  {
+    id: 16,
+    name: "EMG",
+    short_desc:
+      "Electromyography (EMG) is a diagnostic procedure used to assess the health and functionality of muscles and the nerve cells that control them.",
+    image: "/services/emg.png",
+    url: "emg",
+  },
+  {
+    id: 17,
+    name: "Pathology (Blood Tests)",
+    short_desc:
+      "Blood tests are important diagnostic tools that assess overall health, detect diseases, and monitor various health conditions.",
+    image: "/services/pathology.png",
+    url: "pathology",
+  },
+  {
+    id: 18,
+    name: "Blood Pressure Monitoring",
+    short_desc:
+      "Blood pressure monitoring is the process of measuring the force of blood against the walls of your arteries as your heart pumps it through your body.",
+    image: "/services/bloodpressuremonitoring.png",
+    url: "bloodpressuremonitoring",
+  },
+];
---
+++ success.js
@@ -0,0 +1,16 @@
+export const story = [
+  {
+    id: "1",
+    name: "Tubercular cervical lymphadenopathy",
+    vid: "/success/succes1.mp4",
+    content:
+      "A 56 year old female, resident of guwahati, visited delhi for her husband’s surgery. She herself was suffering from right cervical swelling with other symptoms such as weakness, malaise and evening rise of temperature.FNAC was done from cervical swelling but no definite diagnosis was made and her agony persisted.She was referred to us for guided FNAC / biopsy so that we reach definite diagnosis this time.",
+  },
+  {
+    id: "2",
+    vid: "/success/success2.mp4",
+    name: "Understanding of liver abscess",
+    content:
+      "A liver abscess is a pus-filled infection in the liver caused by bacteria, parasites, or fungi. Symptoms include fever, abdominal pain, and nausea. Timely diagnosis through imaging and treatment with antibiotics or drainage is crucial to prevent complications. Always consult a doctor for proper care.",
+  },
+];
